8b234bf7199d408bfbc698de756a66865dc1738b
Alejandro Mora <alejandromora622@gmail.com>
2017-09-26 21:06:32 -0400

    implementing reducer function

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -7,20 +7,20 @@ class ProductList extends Component {
     super()
     this.state = {
       products: [
-        {price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
-        {price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
-        {price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
-        {price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
-        {price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
-        {price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'}
+        {id:'001', price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
+        {id:'002', price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
+        {id:'003', price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
+        {id:'004', price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
+        {id:'005', price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'},
+        {id:'006', price: 200, link: './img/t-shirt-1.jpg', name: 't-shirt', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae impedit inventore odit architecto'}
       ]
     }
   }
   render() {
     return (
     <div>
-      {this.state.products.map( (product, index) =>
-        <Col xs={4} key={index}>
+      {this.state.products.map( product =>
+        <Col xs={4} key={product.id}>
           <Panel bsStyle="info" header={product.name}>
             <Image src={product.link} responsive/>
             <p className="text-justify">{product.desc}</p>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,18 @@
 import { createStore } from 'redux';
 
-function reducer( state = {}, action ) {
+function reducer( state, action ) {
+  if (action.type === "ADD_TO_CART") {
+    return {
+      ...state,
+      cartItems: state.cartItems.concat(action.product)
+    }
+
+  } else if (action.type === "REMOVE_FROM_CART") {
+    return {
+      ...state,
+      cartItems: state.cartItems.filter( product => product.id !== action.product.id)
+    }
+  }
   return state
 }
 
